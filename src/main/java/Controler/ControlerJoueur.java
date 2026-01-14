a6b35b13c8d6c295fdbe6b8bc0ca012a11f77106
isabellebrunie-cmd <isabelle.brunie@gmail.com>
2026-01-14 16:29:14 +0100

    v5

--- src/main/java/Controler/ControlerJoueur.java
+++ src/main/java/Controler/ControlerJoueur.java
@@ -1,14 +1,17 @@
 package Controler;
 
+import Models.Joueur;
 import Views.NouveauJoueur;
 
 public class ControlerJoueur {
 
     public void creerJoueur() {
         //créer un nouveau joueur
-        //appel de la view pour la création du joueur
+        //appel de la view pour un nouveau joueur
         NouveauJoueur GUInouveauJr = new NouveauJoueur();
 
+        //afficher la view pour saisir les infos du nouveau joueur
+        Joueur jou=GUInouveauJr.saisieNouveauJoueur(j);
     }
 
 }
---
+++ src/main/java/Service/ConnectionBDD.java
@@ -0,0 +1,45 @@
+package Service;
+
+import DAO.*;
+import Models.Figurine;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionBDD {
+    private FigurineDAO figurineDAO;
+    private UniteDAO uniteDAO;
+    private ArmeeDAO armeeDAO;
+    private JoueurDAO joueurDAO;
+    private Connection conn;
+
+    public ConnectionBDD() {
+        uniteDAO = new UniteDAOImpl();
+        armeeDAO = new ArmeeDAOImpl();
+        joueurDAO = new JoueurDAOImpl();
+        figurineDAO = new FigurineDAOImpl();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://109.234.166.12:3306/tima6358_nmarchena", "tima6358_nmarchena", "Hayden060819");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Figurine findFig(int id) {
+        Figurine f = null;
+        try {
+            if (conn != null) {
+                f = figurineDAO.findById(id, conn);
+            }
+        } catch (SQLException e) {
+            System.err.format("Erreur SQL:%s \n%s", e.getMessage(), e.getSQLState());
+        }
+        return f;
+    }
+
+}
