821b2c44486e7f94fccf4ed1581f5ecf3fa14a83
isabellebrunie-cmd <isabelle.brunie@gmail.com>
2026-01-14 12:36:57 +0100

    warhammer

--- src/main/java/DAO/ArmeeDAO.java
+++ src/main/java/DAO/ArmeeDAO.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public interface ArmeeDAO {
 
     Armee create (String nomArmee, String faction, Connection c) throws SQLException;
-    Joueur findByPseudo (String pseudoJoueur, Connection c) throws SQLException;
+    Armee findByName (String nomArmee, Connection c) throws SQLException;
     boolean update (String pseudoJoueur, Joueur j, Connection c)throws SQLException;
     boolean delete (String pseudoJoueur, Joueur j, Connection c)throws SQLException;
 }
